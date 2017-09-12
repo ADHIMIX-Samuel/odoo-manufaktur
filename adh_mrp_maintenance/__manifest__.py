f69b5d762f2129598edd2aed7f624adba63a9181
rifat90 <chaerulrifatpauji@gmail.com>
2017-09-12 10:15:10 +0700

    update adh_mrp_maintenance

--- adh_mrp_maintenance/__manifest__.py
+++ adh_mrp_maintenance/__manifest__.py
@@ -1,32 +1,22 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name' : 'Maintenance Management',
-    'version' : '1.1',
-    'summary': '',
-    'sequence': '',
-    'description': """
-==================== 
-    """,
-    'category': 'Manufacturing',
-    'website': 'https://www.adhimix.co.id',
-    'author' :'PT.WITACO-Samuel-Haerul_RP',
-    'email' : 'chaerulrifatpauji@gmail.com',
-    ''
-    'depends' : [
-            'base',
-            'mrp',
-            'report',
-            'product'
-            ],
-    'data': [
+    "name": "Manufaktur - Maintenance", 
+    "version": "1.0", 
+    "depends": [
+        "base",
+        "product",
+        "mrp",
+    ], 
+    "author": "PT.WITACO - Samuel- Haerul",
+    "website": "https://www.adhimix.co.id",
+    "category": "Maintenance List",
+    "summary": "Divisi Manufaktur PT.Adhimix Indonesia",
+    "description": "Module yang digunakan di Divisi Manufaktur",
+    "data": [
         'view/maintenance_view.xml',
         'menu.xml',
-        'data/ir_sequence.xml',
-        
-        
+        'data/ir_sequence.xml'
     ],
-    'installable': True,
-    'application': True,
-    'auto_install': False,
+    "installable": True,
+    "auto_install": False,
+    "application" : True,
 }
